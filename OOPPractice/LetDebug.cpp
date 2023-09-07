ca6b598dda1c1bb70772efcb38ee6fe384ce5af5
배민수 <@MINSU>
2023-09-07 17:28:59 +0900

    초기 커밋.

---
+++ OOPPractice/LetDebug.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class LetDebug
+{
+public:
+	void printNum()
+	{
+		int s1 = 32767;
+		int s2 = 1;
+		int s3 = s1 + s2;
+
+		std::cout << s3 << std::endl;
+	}
+};
+
+int main()
+{
+	LetDebug* id = new LetDebug();
+	id->printNum();
+	return 0;
+}
